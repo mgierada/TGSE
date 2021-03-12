117c5d74245e201d0af13afe13fcab8c0f38d000
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-12 13:37:59 +0100

    fix: fixing missing '/' in endpoint definition

--- assemblyai.py
+++ assemblyai.py
@@ -116,11 +116,12 @@ class SGUTrans:
 
 # SGUTrans().submit('https://media.libsyn.com/media/skepticsguide/skepticast2021-02-27.mp3')
 
-# SGUTrans().get('ic074h0f7-39f8-4550-b347-d645dcb3b2a6')
+SGUTrans().get('ic074h0f7-39f8-4550-b347-d645dcb3b2a6')
+SGUTrans().get_status('ic074h0f7-39f8-4550-b347-d645dcb3b2a6')
 
 # SGUTrans().get('icb47y7t9-2aa0-485e-b8b7-73f0930f7562')
 # SGUTrans().get_status('icb47y7t9-2aa0-485e-b8b7-73f0930f7562')
-SGUTrans().get_transcript('icb47y7t9-2aa0-485e-b8b7-73f0930f7562')
+# SGUTrans().get_transcript('icb47y7t9-2aa0-485e-b8b7-73f0930f7562')
 
 # endpoint = "https://api.assemblyai.com/v2/transcript"
 
