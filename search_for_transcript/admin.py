208805106a7550ed51580fc58b7951604cbcdfd6
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-14 17:59:26 +0100

    fix: clean up

--- search_for_transcript/admin.py
+++ search_for_transcript/admin.py
@@ -4,13 +4,8 @@ from .models import City, Transcript
 # Register your models here.
 
 
-class CityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'state')
-
-
 class TranscriptAdmin(admin.ModelAdmin):
     list_display = ('idd', 'date_published', 'audio_url', 'status')
 
 
-admin.site.register(City, CityAdmin)
 admin.site.register(Transcript, TranscriptAdmin)
