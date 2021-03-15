8066e378611f7a6ad5332c3495579765d3b0c0f4
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-15 09:23:26 +0100

    feat: successfully switched to postgresql

--- search_for_transcript/models.py
+++ search_for_transcript/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=244)
-    state = models.CharField(max_length=244)
+    name = models.CharField(max_length=250)
+    state = models.CharField(max_length=250)
 
     class Meta:
         verbose_name_plural = 'cities'
@@ -13,17 +13,17 @@ class City(models.Model):
 
 
 class Transcript(models.Model):
-    idd = models.CharField(max_length=244)
-    date_published = models.CharField(max_length=244)
-    text = models.CharField(max_length=244)
-    audio_url = models.CharField(max_length=244)
-    status = models.CharField(max_length=244)
+    idd = models.CharField(max_length=240)
+    date_published = models.CharField(max_length=241)
+    text = models.TextField()
+    audio_url = models.CharField(max_length=242)
+    status = models.CharField(max_length=245)
 
     class Meta:
         verbose_name_plural = 'Transcripts'
 
     def __str__(self):
-        return self.audio_url
+        return self.text
 
 
 def populate_db():
