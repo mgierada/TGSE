2de65372afa3eb3b7619ea9d45c33e99aa43fd94
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-15 16:15:40 +0100

    feat: SearchResultsView have now acces to count method

--- search_for_transcript/urls.py
+++ search_for_transcript/urls.py
@@ -5,6 +5,7 @@ from .views import HomePageView, SearchResultsView, CountView
 
 urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search_results'),
+    # path('search/', ShowAllView.as_view(), name='search_results'),
     path('count/', CountView.as_view(), name='count'),
     # path('main/', views.main, name='main'),
     path('', HomePageView.as_view(), name='home')
