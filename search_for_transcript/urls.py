dbf02590a0b4d856f623c5ae65d47985771244c8
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-15 17:30:29 +0100

    fix: cleaning up

--- search_for_transcript/urls.py
+++ search_for_transcript/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView, SearchResultsView, CountView
+from .views import HomePageView, SearchResultsView
 # from . import views
 
 urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search_results'),
-    # path('search/', ShowAllView.as_view(), name='search_results'),
-    path('count/', CountView.as_view(), name='count'),
     # path('main/', views.main, name='main'),
     path('', HomePageView.as_view(), name='home')
 ]
