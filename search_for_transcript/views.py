cc9069df249cba26b3fee4ae1a9e703341bdf3cc
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-15 16:15:40 +0100

    feat: SearchResultsView have now acces to count method

--- search_for_transcript/views.py
+++ search_for_transcript/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
 from django.views.generic import TemplateView, ListView
 from django.views import View
+from typing import Any, Dict
 
 from .models import Transcript
 
@@ -19,6 +20,35 @@ class SearchResultsView(ListView):
         episode_list = Transcript.objects.filter(text__contains=query)
         return episode_list
 
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super(SearchResultsView, self).get_context_data(**kwargs)
+        context['count'] = self.count()
+        return context
+
+
+# class ShowAllView(TemplateView):
+#     template_name = 'search_results.html'
+#     context_object_name = 'episode_list'
+
+#     def get_queryset(self):
+#         query = self.request.GET.get('q')
+#         episode_list = Transcript.objects.filter(text__contains=query)
+#         return episode_list
+
+
+    def count(self):
+        query = self.request.GET.get('q')
+        # query = 'SGU'
+        episode_list = Transcript.objects.filter(text__contains=query)
+        count = 0
+        for episode in episode_list:
+            count += episode.text.count(query)
+        # response = HttpResponse()
+        # response['count'] = count
+        response = 'Found {} occurrences of "{}" in DB'.format(count, query)
+        # return HttpResponse(response)
+        return response
+
 
 class CountView(View):
     model = Transcript
@@ -33,7 +63,7 @@ class CountView(View):
             count += episode.text.count(query)
         response = HttpResponse()
         response['count'] = count
-        response = 'Found {} occurrences of {} in DB'.format(count, query)
+        response = 'Found {} occurrences of "{}" in DB'.format(count, query)
         return HttpResponse(response)
 
     def get_queryset(self):
