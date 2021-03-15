521e0588a2e61f949e3bc9ac68b3f0465168e6e0
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-15 10:51:21 +0100

    feat: basic seatch works

--- search_for_transcript/views.py
+++ search_for_transcript/views.py
@@ -1,5 +1,6 @@
 from typing import List
 from django.views.generic import TemplateView, ListView
+from django.db.models import Q
 
 
 from .models import City, Transcript
@@ -15,10 +16,12 @@ class SearchResultsView(ListView):
 
     def get_queryset(self):
         result = super(SearchResultsView, self).get_queryset()
-        query = self.request.GET.get('search')
-        if query:
-            postresult = Transcript.objects.filter(text__contains=query)
-            result = postresult
-        else:
-            result = None
-        return result
+        query = self.request.GET.get('q')
+        object_list = Transcript.objects.filter(Q(text__contains=query))
+        return object_list
+        # if query:
+        #     postresult = Transcript.objects.filter(text__contains=query)
+        #     result = postresult
+        # else:
+        #     result = None
+        # return result
