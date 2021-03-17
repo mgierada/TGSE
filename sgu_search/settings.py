0a1792e9f775a8d431b158b746fe6d4a1c53976d
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-17 11:39:17 +0100

    feat: adding search bar styling

--- sgu_search/settings.py
+++ sgu_search/settings.py
@@ -91,12 +91,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
