8df1774627f7674f1bb356361268ae8c9ebd4d0b
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-21 13:08:44 +0100

    feat: adding highlight feature to Transcript view

--- search_for_transcript/urls.py
+++ search_for_transcript/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search_results'),
     # path('main/', views.main, name='main'),
     path('', HomePageView.as_view(), name='home'),
-    path('search/transcript/<int:episode_number>/',
+    path('search/transcript/<int:episode_number>/<str:query>',
          TranscriptView.as_view(), name='transcript')
 ]
