9f8de553f97e31ce34c08ada29cfe942172fadd2
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-22 10:46:01 +0100

    feat: adding transcript for ep #818

--- run_trans.py
+++ run_trans.py
@@ -1,14 +1,15 @@
 from transcribe import SGUTrans
 
 sgutrans = SGUTrans()
-links = sgutrans.read_all_podcasts_data_json()[1:2]
-print(links)
-for link in links:
-    sgutrans.submit(link)
+# links = sgutrans.read_all_podcasts_data_json()[1:2]
+# print(links)
+# for link in links:
+#     sgutrans.submit(link)
 
 
 # qxgvpsa0e-42cb-4076-af44-ca5312f4445d
-# SGUTrans().get('qxgvpsa0e-42cb-4076-af44-ca5312f4445d')
+# qxz654b4s-9703-4b7b-b58f-e101b87475b8
+SGUTrans().get('qxgvpsa0e-42cb-4076-af44-ca5312f4445d')
 # # SGUTrans().submit('https://media.libsyn.com/media/skepticsguide/skepticast2021-02-27.mp3')
 
 # SGUTrans().get('ic074h0f7-39f8-4550-b347-d645dcb3b2a6')
