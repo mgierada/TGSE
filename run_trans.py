7d48df7d873cc9c24ff61d3b1e3da27513e7440f
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-22 11:09:37 +0100

    feat: adding two methods acting as a ui for submision and downloading transcripts

--- run_trans.py
+++ run_trans.py
@@ -1,15 +1,22 @@
 from transcribe import SGUTrans
 
 sgutrans = SGUTrans()
-# links = sgutrans.read_all_podcasts_data_json()[1:2]
-# print(links)
+links = sgutrans.read_all_podcasts_data_json()[1:2]
+print(links)
 # for link in links:
 #     sgutrans.submit(link)
 
 
+# ids = sgutrans.get_all_ids_submitted()
+# for id in ids:
+#     print('Getting translation for {}'.format(id))
+#     sgutrans.get(id)
+#     print('Done!')
+# print('All transcripts downloaded successfully')
+
 # qxgvpsa0e-42cb-4076-af44-ca5312f4445d
 # qxz654b4s-9703-4b7b-b58f-e101b87475b8
-SGUTrans().get('qxgvpsa0e-42cb-4076-af44-ca5312f4445d')
+# SGUTrans().get('qxgvpsa0e-42cb-4076-af44-ca5312f4445d')
 # # SGUTrans().submit('https://media.libsyn.com/media/skepticsguide/skepticast2021-02-27.mp3')
 
 # SGUTrans().get('ic074h0f7-39f8-4550-b347-d645dcb3b2a6')
