46dbb779228b48adeb0795fa93dd5e411779eed2
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-23 14:23:36 +0100

    add: episodes up to #770 sent for transcription

--- run_trans.py
+++ run_trans.py
@@ -1,5 +1,5 @@
 from transcribe import SGUTrans
 
 sgutrans = SGUTrans()
-# sgutrans.submit_all_transcripts(first_episode=790, last_episode=806)
+# sgutrans.submit_all_transcripts(first_episode=770, last_episode=789)
 sgutrans.downlad_all_transcripts()
