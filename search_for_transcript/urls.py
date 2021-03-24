e0e81cf773ab7c04c8756a17d872fb2a38a36e42
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-24 10:49:26 +0100

    feat: addding plain transcript and improved styling

--- search_for_transcript/urls.py
+++ search_for_transcript/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from .views import HomePageView, SearchResultsView, TranscriptView
-# from . import views
+from .views import (HomePageView,
+                    SearchResultsView,
+                    TranscriptView,
+                    TranscriptPlainView)
 
 urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search_results'),
     # path('main/', views.main, name='main'),
     path('', HomePageView.as_view(), name='home'),
     path('search/transcript/<int:episode_number>/<str:query>',
-         TranscriptView.as_view(), name='transcript')
+         TranscriptView.as_view(), name='transcript'),
+    path('search/transcript_plain/<int:episode_number>/<str:query>',
+         TranscriptPlainView.as_view(), name='transcript_plain')
 ]
