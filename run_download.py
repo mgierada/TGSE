b59cc5e04ca9602212091eba1fd319a4de268cef
Maciej Gierada <maciek.gierada@gmail.com>
2021-03-30 14:36:49 +0200

    feat: adding run_download.py and run_submit.py

--- run_download.py
+++ run_download.py
@@ -1,5 +1,4 @@
 from transcribe import SGUTrans
 
 sgutrans = SGUTrans()
-# sgutrans.submit_all_transcripts(first_episode=650, last_episode=679)
 sgutrans.downlad_all_transcripts()
---
+++ run_submit.py
@@ -0,0 +1,4 @@
+from transcribe import SGUTrans
+
+sgutrans = SGUTrans()
+sgutrans.submit_all_transcripts(first_episode=249, last_episode=299)
