734c220d94696c5657d7bfe3e5f583937d47ee12
Maciej Gierada <maciek.gierada@gmail.com>
2021-04-07 08:52:23 +0200

    fix: for local runs, set debug=True

--- sgu_search/settings.py
+++ sgu_search/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['sgu_search_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sgu-search.herokuapp.com', '.herokuapp.com',
                  'localhost', '127.0.0.1', ]
