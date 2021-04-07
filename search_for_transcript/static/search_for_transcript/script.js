2101cbfa4365d2015c817d237197f624ba897972
Maciej Gierada <maciek.gierada@gmail.com>
2021-04-07 10:21:50 +0200

    fix: element declaration in scropt.js

--- search_for_transcript/static/search_for_transcript/script.js
+++ search_for_transcript/static/search_for_transcript/script.js
@@ -1,5 +1,5 @@
-function click_listen_button(episode_number){
-    element = document.getElementById('listen_button' + String(episode_number));
+function click_listen_button(episode_number) {
+    let element = document.getElementById('listen_button' + String(episode_number));
     return element.click()
 }
 
