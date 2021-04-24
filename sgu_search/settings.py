1e8a11f614c3d1583a145fdedc03dac5ced79484
Maciej Gierada <maciek.gierada@gmail.com>
2021-04-24 11:19:53 +0200

    fix: removing debug_toolbar mentions

--- sgu_search/settings.py
+++ sgu_search/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'search_for_transcript',
-    'debug_toolbar'
 ]
 
 
@@ -60,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'sgu_search.urls'
